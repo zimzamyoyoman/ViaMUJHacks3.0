190852338a1a801f613483e971963e787a1a30d9
Samarth Goyal <33006915+zimzamyoyoman@users.noreply.github.com>
2019-09-22 13:38:01 +0530

    Add files via upload

---
+++ Arduino_Code_-_Distance_Measurement/Arduino_Code_-_Distance_Measurement.ino
@@ -0,0 +1,61 @@
+int trigPin=13;
+int echoPin=9;
+int piezoPin=7;
+int ledPin=8;
+float pingTime;
+float targetDistance;
+float speedOfSound = 776.5; //Speed of sound in meters per second
+float notes[]={493.883,349.228,261.626,};
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(piezoPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2000);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(20);
+
+  pingTime=pulseIn(echoPin, HIGH);
+//  Serial.println(pingTime);  
+  pingTime=pingTime/1000000.0; //Converts microseconds to seconds
+  pingTime=pingTime/3600;
+//  Serial.println(pingTime); 
+  targetDistance=63360*speedOfSound*pingTime/2; //Store target distance in inches
+
+  if (targetDistance < 5) {
+      tone(piezoPin,notes[0],100);
+      digitalWrite(ledPin, HIGH);
+      delay(25);
+      digitalWrite(ledPin, LOW);
+    }
+  else if (targetDistance < 10) {
+      tone(piezoPin,notes[1],100);
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+      digitalWrite(ledPin, LOW);
+    }
+  else if (targetDistance < 15) {
+      tone(piezoPin,notes[2],100);
+      digitalWrite(ledPin, HIGH);
+      delay(400);
+      digitalWrite(ledPin, LOW);         
+  }
+  else if (targetDistance < 20) {
+      tone(piezoPin, notes[3], 100);
+  }
+
+  else if (targetDistance < 25) {
+    tone(piezoPin,notes[3],100);
+  }
+  Serial.println(targetDistance);
+  delay(350);
+}
+
---
+++ NodeMCU_Code_-_ThingSpeak_Upload/NodeMCU_Code_-_ThingSpeak_Upload.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+
+String apiWritekey = "K4LDXVED5EH1NG7D"; // replace with your THINGSPEAK WRITEAPI key here
+const char* ssid = "ZENFONE"; // your wifi SSID name
+const char* password = "qwertyuiop" ;// wifi pasword
+ 
+const char* server = "api.thingspeak.com";
+const int trigPin = 14;  //D5
+const int echoPin = 12;  //D6
+
+long duration;
+int distance;
+
+WiFiClient client;
+ 
+void setup() {
+  Serial.begin(115200);
+  WiFi.disconnect();
+  delay(10);
+  WiFi.begin(ssid, password);
+ 
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("NodeMcu connected to wifi...");
+  Serial.println(ssid);
+  Serial.println();
+}
+ 
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance= duration*0.034/2;
+
+  delay(2000);
+    
+  if (client.connect(server,80))
+  {  
+    String tsData = apiWritekey;
+           tsData +="&field1=";
+           tsData += String(distance);
+           tsData += "\r\n\r\n";
+ 
+     client.print("POST /update HTTP/1.1\n");
+     client.print("Host: api.thingspeak.com\n");
+     client.print("Connection: close\n");
+     client.print("X-THINGSPEAKAPIKEY: "+apiWritekey+"\n");
+     client.print("Content-Type: application/x-www-form-urlencoded\n");
+     client.print("Content-Length: ");
+     client.print(tsData.length());
+     client.print("\n\n");  // the 2 carriage returns indicate closing of Header fields & starting of data
+     client.print(tsData);
+ 
+     Serial.print("Distance: ");
+     Serial.print(distance);
+     Serial.println("uploaded to Thingspeak server....");
+  }
+  client.stop();
+ 
+  Serial.println("Waiting to upload next reading...");
+  Serial.println();
+  // thingspeak needs minimum 15 sec delay between updates
+  delay(15000);
+}
+
